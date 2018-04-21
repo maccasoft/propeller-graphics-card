af1cfeb89b169723532784159e43d44a83fddfe6
Marco Maccaferri <marco.maccaferri@maccasoft.com>
2018-04-21 12:03:13 +0200

    Unified bitmap bus interface code

--- bitmap_bus_interface_256x224.s
+++ bitmap_bus_interface_256x224.s
@@ -1,32 +1,9 @@
-/*
- * Propeller Graphics Card for RC2014 Computer
- *
- * I/O Addres line mapping:
- *
- *           P8 P7 P6 P5 P4 P3 P2 P1 P0
- *            -  1  1  0  x  x  1  0  0
- *            |  |  |  |  |  |  |  |  |
- *      D0 ---+  |  |  |  |  |  |  |  |
- *      M1 ------+  |  |  |  |  |  |  |
- *      RD ---------+  |  |  |  |  |  |
- *      WR ------------+  |  |  |  |  |
- *      A1 ---------------+  |  |  |  |
- *      A0 ------------------+  |  |  |
- *      A6 ---------------------+  |  |
- *      A7,A5,A4,A3,A2 ------------+  |
- *      IORQ -------------------------+
- *
- *      P8 must be masked out (data dbus line)
- *
- * Copyright (c) 2018 Marco Maccaferri
- * MIT Licensed
- *
- */
 
 #include "defines.inc"
 
 #define H_RES   256
 #define V_RES   224
+#define BPP     4
 
                         .pasm
                         .compress off
